8e2f3d4e0271e65f61a5a46b9539209fdb6cc0d7
mehmettasan <83862655+mehmettasan@users.noreply.github.com>
2021-05-29 21:18:02 +0300

    ws3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const databasae=require('./database');
 const ejs = require('ejs');
 const postController=require('./controller/postController');
+const pageController=require('./controller/pageController');
 
 const app = express();
 
@@ -15,15 +16,14 @@ app.use(express.json());
 
 //route
 app.get('/', postController.getAllPost)
-app.get('/add_post', (req, res) => {
-  res.render('add_post')
-})
+app.get('/add_post',pageController.addPost)
 app.post('/cpost',postController.createPost)
-app.get('/post', (req, res) => {
-  res.render('post')
-})
-app.get('/about', (req, res) => {
-  res.render('about')
+app.get('/about',pageController.about)
+app.get('/posts/:id',pageController.showPost)
+
+app.get('*',(req, res)=>{
+  res.status(404)
+  res.send('404 Sayfa bulunamadı...')
 })
 
 const port = 3000;
--- controller/pageController.js
+++ controller/pageController.js
@@ -1,2 +1,16 @@
 const Post = require('../model/Post');
 
+exports.showPost=async(req, res)=>{
+    const post= await Post.findById(req.params.id)
+    res.render('post',{
+        post,
+      })
+       console.log(post)
+}
+exports.about= (req, res) => {
+    res.render('about')
+  }
+
+  exports.addPost= (req, res) => {
+    res.render('add_post')
+  }
